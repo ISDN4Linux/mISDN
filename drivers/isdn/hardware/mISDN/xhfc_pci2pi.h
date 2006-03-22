f75bf726441ecfaf83a64fc3325d6758e7b5eff6
keil <keil>
2006-03-22 23:29:59 +0000

    fix typo

--- drivers/isdn/hardware/mISDN/xhfc_pci2pi.h
+++ drivers/isdn/hardware/mISDN/xhfc_pci2pi.h
@@ -1,4 +1,4 @@
-/* $Id: xhfc_pci2pi.h,v 1.3 2006/03/06 16:20:33 mbachem Exp $
+/* $Id: xhfc_pci2pi.h,v 1.4 2006/03/22 23:29:59 keil Exp $
  *
  * PCI2PI Pci Bridge support for xhfc_su.c
  *
@@ -7,7 +7,7 @@
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; eit_her version 2, or (at your option)
+ * the Free Software Foundation; either version 2, or (at your option)
  * any later version.
  *
  * This program is distributed in the hope that it will be useful,
