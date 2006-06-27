d475730b7b461900313a84d3438b987b43e84c18
crich <crich>
2006-06-27 09:55:17 +0000

    for now we release only all the ports when the module is unloaded. It doesn't make really a lot of sense to release single ports on the hfcmulti cards. The better way would be to relase the ports from the queue instead. But the queue knows only about single stacks .. I propose to add a MODULE_REGISTER and MODULE_RELEASE to the mqueue, this one cleans up all the necessary stuff and we only send a MODULE_RELEASE from within the module_unload function. For now this fix should work in most cases.

--- drivers/isdn/hardware/mISDN/hfc_multi.c
+++ drivers/isdn/hardware/mISDN/hfc_multi.c
@@ -124,7 +124,7 @@ static void ph_state_change(channel_t *ch);
 
 extern const char *CardType[];
 
-static const char *hfcmulti_revision = "$Revision: 1.43 $";
+static const char *hfcmulti_revision = "$Revision: 1.44 $";
 
 static int HFC_cnt, HFC_idx;
 
@@ -3197,8 +3197,10 @@ bugtest
 		case MGR_RELEASE | INDICATION:
 		if (debug & DEBUG_HFCMULTI_MGR)
 			printk(KERN_DEBUG "%s: MGR_RELEASE = remove port from mISDN\n", __FUNCTION__);
+#if 0
 		if (test_bit(FLG_DCHANNEL, &chan->Flags))
 			release_port(hc, hc->chan[ch].port); /* hc is free */
+#endif
 		break;
 #ifdef FIXME
 		case MGR_CONNECT | REQUEST:
