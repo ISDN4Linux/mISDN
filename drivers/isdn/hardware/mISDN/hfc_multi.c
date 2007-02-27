edb655b9089bda171ca2b9b4b4874d82f3178e7e
crich <crich>
2007-02-27 21:16:08 +0000

    added junghanns pci id (quadbri 2.0 board)

--- drivers/isdn/hardware/mISDN/hfc_multi.c
+++ drivers/isdn/hardware/mISDN/hfc_multi.c
@@ -128,7 +128,7 @@ static void ph_state_change(channel_t *ch);
 
 extern const char *CardType[];
 
-static const char *hfcmulti_revision = "$Revision: 1.64 $";
+static const char *hfcmulti_revision = "$Revision: 1.65 $";
 
 static int HFC_cnt;
 
@@ -191,6 +191,8 @@ static const PCI_ENTRY id_list[] =
 	 "HFC-4S IOB4ST", 0, 4, 1, 2},
 	{CCAG_VID, CCAG_VID, HFC4S_ID, 0xB620, VENDOR_CCD,
 	 "HFC-4S", 0, 4, 1, 2},
+	{CCAG_VID, CCAG_VID, HFC4S_ID, 0xB550, VENDOR_CCD,
+	 "HFC-4S (junghanns 2.0)", 0, 4, 1, 2},
 	{CCAG_VID, CCAG_VID, HFC4S_ID, 0xB560, VENDOR_CCD,
 	 "HFC-4S Beronet Card", 0, 4, 1, 2},
 	{CCAG_VID, CCAG_VID, HFC4S_ID, 0xB568, VENDOR_CCD,
@@ -4362,6 +4364,7 @@ static struct pci_device_id hfmultipci_ids[] __devinitdata = {
 	{ CCAG_VID, 0x08B4   , CCAG_VID, 0xB620, 0, 0, 0 }, //4S
 	{ 0xD161, 0xB410   , 0xD161, 0xB410, 0, 0, 0 }, //4S - Digium
 	{ CCAG_VID, 0x08B4   , CCAG_VID, 0xB540, 0, 0, 0 }, //4S Swyx
+	{ CCAG_VID, 0x08B4   , CCAG_VID, 0xB550, 0, 0, 0 }, //4S junghanns 2.0
 	
 	/** Cards with HFC-8S Chip**/
 	{ CCAG_VID, 0x16B8   , CCAG_VID, 0xB562, 0, 0, 0 }, //BN8S
