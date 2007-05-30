12988c9fca6b3248a6a1d46052fa1345a569f50b
Karsten Keil <kkeil@suse.de>
2007-05-30 14:36:23 +0200

    more style changes

--- drivers/isdn/hardware/mISDN/dsp_octwareec.h
+++ drivers/isdn/hardware/mISDN/dsp_octwareec.h
@@ -1,15 +1,15 @@
-/*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\
-
-File:  octasicec14.h
-
-Copyright (c) 2007 Octasic Inc. All rights reserved.
-
-Description: 
-
-    This file contains the functions used as the entry point to Octasic's
-    echo cancellation software.
-
-\*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
+/*
+ *
+ * File:  octasicec14.h
+ *
+ * Copyright (c) 2007 Octasic Inc. All rights reserved.
+ *
+ * Description:
+ *
+ *  This file contains the functions used as the entry point to Octasic's
+ *  echo cancellation software.
+ *
+ */
 
 #ifndef __OCTWARE_EC_H__
 #define __OCTWARE_EC_H__
@@ -22,7 +22,7 @@ Description:
 #if 0
 static void echo_can_init(void)
 {
-	printk( "Zaptel Echo Canceller: OCTVQE\n" );
+	printk(KERN_INFO "Zaptel Echo Canceller: OCTVQE\n");
 }
 
 static void echo_can_shutdown(void)
@@ -32,52 +32,57 @@ static void echo_can_shutdown(void)
 
 struct echo_can_state
 {
-    void * pvOctvqeEchoCanceller;
+	void	*pvOctvqeEchoCanceller;
 };
 
-static inline struct echo_can_state *echo_can_create(int len, int adaption_mode)
+static inline struct echo_can_state
+*echo_can_create(int len, int adaption_mode)
 {
-    struct echo_can_state *pEchoCanceller = NULL;
-	
-    /* Allocate echo canceller state structure. */
-    pEchoCanceller = (struct echo_can_state *) kmalloc(sizeof(struct echo_can_state), GFP_KERNEL);
-    if (pEchoCanceller != NULL)
-    {
-        pEchoCanceller->pvOctvqeEchoCanceller = ZapOctVqeApiEcChannelInitialize(len, adaption_mode);
-        if (pEchoCanceller->pvOctvqeEchoCanceller == NULL)
-        {
-            printk( KERN_ERR "ZapOctVqeApiEcChannelInitialize failure!\n" );
+	struct echo_can_state *pEchoCanceller = NULL;
 
-            /* Cleanup. */
-            kfree(pEchoCanceller);
-            pEchoCanceller = NULL;
-        }
-    }
-    else
-    {
-        printk( KERN_ERR
-            "echo_can_create error: kmalloc failed (requested %ld bytes)\n", 
-            sizeof(struct echo_can_state));
-    }
-	
+	/* Allocate echo canceller state structure. */
+	pEchoCanceller = kmalloc(sizeof(struct echo_can_state), GFP_KERNEL);
+	if (pEchoCanceller != NULL) {
+		pEchoCanceller->pvOctvqeEchoCanceller =
+		    ZapOctVqeApiEcChannelInitialize(len, adaption_mode);
+		if (pEchoCanceller->pvOctvqeEchoCanceller == NULL) {
+			printk(KERN_ERR
+			    "ZapOctVqeApiEcChannelInitialize failure!\n");
+
+			    /* Cleanup. */
+			kfree(pEchoCanceller);
+			pEchoCanceller = NULL;
+		}
+	} else {
+		printk(KERN_ERR
+		    "echo_can_create error: kmalloc failed "
+		    "(requested %ld bytes)\n",
+		    sizeof(struct echo_can_state));
+	}
+
 	return pEchoCanceller;
 }
 
-static inline short echo_can_update(struct echo_can_state *ec, short iref, short isig)
+static inline short
+echo_can_update(struct echo_can_state *ec, short iref, short isig)
 {
-    return ZapOctVqeApiEcChannelProcess(ec->pvOctvqeEchoCanceller, iref, isig);
+	return ZapOctVqeApiEcChannelProcess(ec->pvOctvqeEchoCanceller,
+	    iref, isig);
 }
 
-static inline void echo_can_free(struct echo_can_state *ec)
+static inline void
+echo_can_free(struct echo_can_state *ec)
 {
 	/* Tell OCTVQE module that this channel is not used anymore. */
-    ZapOctVqeApiEcChannelFree(ec->pvOctvqeEchoCanceller);
-    kfree(ec);
+	ZapOctVqeApiEcChannelFree(ec->pvOctvqeEchoCanceller);
+	kfree(ec);
 }
 
-static inline int echo_can_traintap(struct echo_can_state *ec, int pos, short val)
-{	
-	return ZapOctVqeApiEcChannelTrainTap(ec->pvOctvqeEchoCanceller, pos, val);
+static inline int
+echo_can_traintap(struct echo_can_state *ec, int pos, short val)
+{
+	return ZapOctVqeApiEcChannelTrainTap(ec->pvOctvqeEchoCanceller,
+	    pos, val);
 }
- 
+
 #endif /* __OCTWARE_EC_H__ */
