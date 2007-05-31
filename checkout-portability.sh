29b807f5580f27468c4f6feed4428401b6e9dcfe
Christian <crich@saruman.(none)>
2007-05-31 13:48:30 +0200

    modded portability checkout script

--- checkout-portability.sh
+++ checkout-portability.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #checks out the mISDN_1_1 branch
-git-checkout -b mISDN_1_1 origin/mISDN_1_1
-git-config branch.mISDN_1_1.remote origin
-git-config branch.mISDN_1_1.merge refs/heads/mISDN_1_1
+git-checkout -b portability origin/portability
+git-config branch.portability.remote origin
+git-config branch.portability.merge refs/heads/portability
