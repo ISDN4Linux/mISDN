a53e9be22115c5a7a6c6a3356c139df701bd7d20
Martin Bachem <m.bachem@gmx.de>
2008-09-03 18:08:30 +0200

    [MISDN] Use protocol to detect D-channel Use protocol to detect D-channel not the channel number 0
    Signed-off-by: Martin Bachem <m.bachem@gmx.de>

--- drivers/isdn/hardware/mISDN/hfcpci.c
+++ drivers/isdn/hardware/mISDN/hfcpci.c
@@ -1966,7 +1966,8 @@ hfc_dctrl(struct mISDNchannel *ch, u_int cmd, void *arg)
 	switch (cmd) {
 	case OPEN_CHANNEL:
 		rq = arg;
-		if (rq->adr.channel == 0)
+		if ((rq->protocol == ISDN_P_TE_S0) ||
+		    (rq->protocol == ISDN_P_NT_S0))
 			err = open_dchannel(hc, ch, rq);
 		else
 			err = open_bchannel(hc, rq);
