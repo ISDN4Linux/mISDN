d17cde02680fe08d197e37d5396388e50e2873b2
Karsten Keil <kkeil@linux-pingi.de>
2012-08-23 09:42:24 +0200

    Revert "Add PCM only B-channel protocol to socket functions"
    This reverts commit 78d7a96a90c579748e82c862fc957da8a7b63003.

--- drivers/isdn/mISDN/socket.c
+++ drivers/isdn/mISDN/socket.c
@@ -270,7 +270,6 @@ data_sock_release(struct socket *sock)
 		break;
 	case ISDN_P_LAPD_TE:
 	case ISDN_P_LAPD_NT:
-	case ISDN_P_B_PCM:
 	case ISDN_P_B_RAW:
 	case ISDN_P_B_HDLC:
 	case ISDN_P_B_X75SLP:
@@ -802,7 +801,6 @@ mISDN_sock_create(struct net *net, struct socket *sock, int proto, int kern)
 	case ISDN_P_NT_E1:
 	case ISDN_P_LAPD_TE:
 	case ISDN_P_LAPD_NT:
-	case ISDN_P_B_PCM:
 	case ISDN_P_B_RAW:
 	case ISDN_P_B_HDLC:
 	case ISDN_P_B_X75SLP:
